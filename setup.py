9e67170ee6177d866f652806089c0dbf2af33b09
Dhanush <dhanush01bhandary@gmail.com>
2026-07-06 07:54:29 +0530

    project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="src",
+    version="0.01",
+    author="Dhanush",
+    author_email="dhanush01bhandary@gmail.com",
+    packages=find_packages(),
+
+)
