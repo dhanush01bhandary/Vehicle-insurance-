7fba2a02a620194728e219f1d538ceb6f1f8683b
Dhanush <dhanush01bhandary@gmail.com>
2026-07-28 09:52:24 +0530

    till data validation

--- pokkade.py
+++ pokkade.py
@@ -1,5 +1,15 @@
-import pandas as pd
-import pymongo
+from pymongo import MongoClient
 
-df=pd.read_csv('data.csv')
-df.head()
+client = MongoClient("mongodb+srv://dhanush01bhandary_db_user:UjkeLI49xKyGQh1B@cluster0.tkp8cl7.mongodb.net/?appName=Cluster0")
+
+print(client.list_database_names())
+
+db = client["Proj1"]
+print(db.list_collection_names())
+
+collection = db["Proj1_Data"]
+
+print("Document count:", collection.count_documents({}))
+
+doc = collection.find_one()
+print(doc)
