e4277a42eeee1747fc509e25ca76bdadbd21ac3b
Dhanush <dhanush01bhandary@gmail.com>
2026-07-30 21:18:54 +0530

    deployed4

--- app.py
+++ app.py
@@ -136,8 +136,9 @@ async def predictRouteClient(request: Request):
 
         # Render the same HTML page with the prediction result
         return templates.TemplateResponse(
-            "vehicledata.html",
-            {"request": request, "context": status},
+            request=request,
+            name="vehicledata.html",
+            context={"context": status},
         )
         
     except Exception as e:
